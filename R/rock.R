c785cd257400de20d76fc9d9ff9f05419b3e02b5
LangeTo <tobias.hundertmark@dkfz-heidelberg.de>
2026-03-04 23:18:50 +0100

    Initial commit.

---
+++ R/rock.R
@@ -0,0 +1,29 @@
+#' @title Fill Collins
+#'
+#' @description
+#' Fill a collin!
+#'
+#' @param .data A data frame to be modified.
+#'
+#' @param ... Additional arguments to be passed to the mutate function.
+#'
+#' @export
+fill_collins <- function(.data, ...) {
+  message("\nI can feel coming in the air tonight!\n")
+  mutate(.data, ...)
+}
+
+#' @title Phil columns
+#'
+#' @description
+#' Phil a column!
+#'
+#' @param .data A data frame to be modified.
+#'
+#' @param ... Additional arguments to be passed to the mutate function.
+#'
+#' @export
+phil_columns <- function(.data, ...) {
+  message("\nI can feel coming in the air tonight!\n")
+  mutate(.data, ...)
+}
